1b5027bfa338a3d86f49dfbd00506c2b05379c86
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-16 11:20:09 +1200

    Add request logging

--- main.ts
+++ main.ts
@@ -2,6 +2,21 @@ import { Application, Context } from 'https://deno.land/x/oak/mod.ts';
 
 const app = new Application();
 
+// Logger
+app.use(async (ctx: Context, next: Function) => {
+  await next();
+  const rt = ctx.response.headers.get('X-Response-Time');
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx: Context, next: Function) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set('X-Response-Time', `${ms}ms`);
+});
+
 app.use((ctx: Context) => {
   ctx.response.body = { message: 'Hello World!' };
 });
